ea16c6e0f46427cdb16085d6b9afbab5d54b7d16
CapSap <cshotam@gmail.com>
2020-12-19 21:22:20 +1100

    tidy registration

--- src/registration.js
+++ src/registration.js
@@ -1,14 +1,15 @@
-function Registration () {
-  return(
-    <div> 
-<div> 
-  <form>
-    <input type='text'></input>
-    <input type='text'></input>
-    <input type="submit" value="Submit"></input>
-  </form>
-  </div>    </div>
-  )
+function Registration() {
+  return (
+    <div>
+      <div>
+        <form>
+          <input type="text"></input>
+          <input type="text"></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Registration;
