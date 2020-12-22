ffa0f375cc30008e6a8a68b9d7c50df00a5b99c0
CapSap <cshotam@gmail.com>
2020-12-22 18:55:09 +1100

    vote count working

--- src/components/Project-lists.js
+++ src/components/Project-lists.js
@@ -4,39 +4,48 @@ import Projects from "./Projects";
 class Projectlist extends Component {
   state = {
     project: [
-      
       {
         id: 1,
-        title: 'Project Green Kangaroo',
-        decription: 'Project Green Kangaroo is a sustainable investment solution which allows investors to align their financial objectives with net zero emission targets, while supporting the financing of low emissions energy transition.',
-        link: 'https://australianclimatetransition.bnpparibas.com/#',
+        title: "Project Green Kangaroo",
+        decription:
+          "Project Green Kangaroo is a sustainable investment solution which allows investors to align their financial objectives with net zero emission targets, while supporting the financing of low emissions energy transition.",
+        link: "https://australianclimatetransition.bnpparibas.com/#",
         votecount: 0,
-
       },
       {
         id: 2,
-        title: 'Fight For Our Reef',
-        decription: 'Our beautiful Great Barrier Reef is one of the world’s most biologically diverse ecosystems - home to turtles, corals and clownfish like Nemo. But its future is threatened by colossal new coal mines, carbon pollution and creeping industrialisation.',
-        link: 'https://www.marineconservation.org.au/fight-for-our-reef/',
+        title: "Fight For Our Reef",
+        decription:
+          "Our beautiful Great Barrier Reef is one of the world’s most biologically diverse ecosystems - home to turtles, corals and clownfish like Nemo. But its future is threatened by colossal new coal mines, carbon pollution and creeping industrialisation.",
+        link: "https://www.marineconservation.org.au/fight-for-our-reef/",
         votecount: 0,
-
       },
       {
         id: 3,
-        title: 'Project Lorem ipsum',
-        decription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        link: 'www.inpellentesquemassa.org',
+        title: "Project Lorem ipsum",
+        decription:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        link: "www.inpellentesquemassa.org",
         votecount: 0,
+      },
+    ],
+  };
 
-      }
-    ]
-  }
+  handleClick = (id) => {
+    this.setState({
+      project: this.state.project.map((project) => {
+        if (project.id === id) {
+          project.votecount += 1;
+        }
+        return project;
+      }),
+    });
+  };
+
   render() {
-  return (
-    this.state.project.map((x) => (
-      <Projects item={x}/>
-    ))
-  )
+    return this.state.project.map((x) => (
+      <Projects item={x} handleClick={this.handleClick} />
+    ));
   }
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,15 @@
 import { Component } from "react";
 
 class Projects extends Component {
+  
   render() {
     return (
       <div className='projects'>
         {this.props.item.title} 
         <br></br>
-        
-        <button>Vote</button>
+        <button onClick={this.props.handleClick.bind(this, this.props.item.id)}>
+          Vote
+          </button>
         {this.props.item.votecount}
       </div>
     );
