bdcc0c0a9a9b2d5e95989ab12c5610e6ff0d2dd8
CapSap <cshotam@gmail.com>
2020-12-22 22:37:09 +1100

    m

--- src/components/Project-lists.js
+++ src/components/Project-lists.js
@@ -7,7 +7,7 @@ class Projectlist extends Component {
       {
         id: 1,
         title: "Project Green Kangaroo",
-        decription:
+        description:
           "Project Green Kangaroo is a sustainable investment solution which allows investors to align their financial objectives with net zero emission targets, while supporting the financing of low emissions energy transition.",
         link: "https://australianclimatetransition.bnpparibas.com/#",
         votecount: 0,
@@ -15,7 +15,7 @@ class Projectlist extends Component {
       {
         id: 2,
         title: "Fight For Our Reef",
-        decription:
+        description:
           "Our beautiful Great Barrier Reef is one of the world’s most biologically diverse ecosystems - home to turtles, corals and clownfish like Nemo. But its future is threatened by colossal new coal mines, carbon pollution and creeping industrialisation.",
         link: "https://www.marineconservation.org.au/fight-for-our-reef/",
         votecount: 0,
@@ -23,7 +23,7 @@ class Projectlist extends Component {
       {
         id: 3,
         title: "Project Lorem ipsum",
-        decription:
+        description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         link: "www.inpellentesquemassa.org",
         votecount: 0,
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,10 @@ class Projects extends Component {
     return (
       <div className='projects'>
         {this.props.item.title} 
-        <br></br>
+        <p>
+          {this.props.item.description}
+        </p>
+        <a href={this.props.item.link}>Click here for more info</a>        <br></br>
         <button 
         onClick={this.props.handleClick.bind(this, this.props.item.id)}>
           Vote
