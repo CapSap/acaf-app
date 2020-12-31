2f2eced901368cf8bbc92be56f83f41638026039
CapSap <cshotam@gmail.com>
2020-12-31 16:37:24 +1100

    tweaking registration css

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,15 +1,17 @@
+import '../App.css';
+
 function Registration() {
   return (
-    <div>
-      <div>
-        <form>
-          <input type="text" placeholder="Your first name" required="true"></input>
-          <input type="text" placeholder="Your last name" required="true" ></input>          
-          <input type="email" placeholder="Your email addres" required="true"></input>
+      <div className="registration-container">
+         
+        <form className="registration-forms-content">
+        <h2>Register Here</h2>
+          <input className="input-box" type="text" placeholder="Your first name" required="true"></input>
+          <input className="input-box" type="text" placeholder="Your last name" required="true" ></input>          
+          <input className="input-box" type="email" placeholder="Your email addres" required="true"></input>
           <input type="submit" value="Submit"></input>
         </form>
       </div>
-    </div>
   );
 }
 
