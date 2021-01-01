4eb81f52735fb790d93d918baa00bce378d6fe60
CapSap <cshotam@gmail.com>
2021-01-01 15:09:31 +1100

    fixing up the register title and form boxes

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -3,13 +3,13 @@ import '../App.css';
 function Registration() {
   return (
       <div className="registration-container">
-         
+        <h2 className="registration-heading">Register Here</h2>
+
         <form className="registration-forms-content">
-        <h2>Register Here</h2>
           <input className="input-box" type="text" placeholder="Your first name" required="true"></input>
           <input className="input-box" type="text" placeholder="Your last name" required="true" ></input>          
           <input className="input-box" type="email" placeholder="Your email addres" required="true"></input>
-          <input className="input-box" type="submit" value="Submit"></input>
+          <input className="input-box"  type="submit" value="Submit"></input>
         </form>
       </div>
   );
